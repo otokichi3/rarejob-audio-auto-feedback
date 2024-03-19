f1f6b8e42e90b3d1c7e51a50a548b40f89584792
otokichi3 <kofunital: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@gmail.com>
2024-03-19 22:36:14 +0900

    feat: use Gemini

---
+++ README.md
@@ -0,0 +1,4 @@
+# rarejob-auto-feedback
+
+## API の有効化
+https://console.developers.google.com/apis/api/aiplatform.googleapis.com/overview?project=$PROJECT_ID
--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 import requests
 import os
+import vertexai
+from vertexai.generative_models import GenerativeModel, ChatSession
 from bs4 import BeautifulSoup
 from openai import OpenAI
 
 FILENAME = "audio.mp3"
 
 
-def get_audio_content():
+
+def get_audio_text():
     session = requests.Session()
 
     # ログインする
     rj_id = os.getenv("RJ_ID")
     rj_password = os.getenv("RJ_PASSWORD")
-    print(rj_id)
-    print(rj_password)
     res_top = session.post(
-        url="https://www.rarejob.com/account/login/",
+        url=os.getenv("LOGIN_URL"),
         data={
             "RJ_LoginForm[email]": rj_id,
             "RJ_LoginForm[password]": rj_password,
@@ -29,7 +30,7 @@ def get_audio_content():
     soup_detail = BeautifulSoup(res_detail.text, "html.parser")
     audio_data = soup_detail.find("audio", class_="audioData")
 
-    # mp3をダウンロードする
+    # mp3を取得する
     content = session.get(audio_data.get("src")).content
 
     with open(FILENAME, "wb") as f:
@@ -43,15 +44,40 @@ def get_audio_content():
             response_format="verbose_json",
             timestamp_granularities=["segment"],
         )
+        text = ""
         for s in transcription.segments:
-            print(s["text"])
-    # TODO text to Gemini API to improve student's English
+            text += s["text"]
+
+    return text
 
 
-"""
-Please identify the speakers and add "tutor:" to the teacher's utterances and "you:" to the student's utterances for the following text.
-TODO: add suggestions
-"""
+def get_chat_response(chat: ChatSession, prompt: str) -> str:
+    text_response = []
+    responses = chat.send_message(prompt, stream=True)
+    for chunk in responses:
+        text_response.append(chunk.text)
+    return "".join(text_response)
+
+def get_imporved_sentences(text: str) -> str:
+    vertexai.init(project=os.getenv("PROJECT_ID"), location=os.getenv("LOCATION"))
+    model = GenerativeModel("gemini-1.0-pro")
+    chat = model.start_chat()
+
+    prompt_identified = "Please identify the speakers and add \"tutor:\" to the teacher's utterances and add \"you:\" to the student's utterances for the following text."
+    prompt_identified = prompt_identified + "\n" + "---" + "\n"
+    prompt_identified = prompt_identified + text
+    text_identified = get_chat_response(chat, prompt_identified)
+
+    prompt_improved = "Please improve the following sentences to be more natural with reason for revision. Please keep an original sentence while appending an improved sentence in each line."
+    prompt_improved = prompt_improved + "\n" + "---" + "\n"
+    prompt_improved = prompt_improved + text_identified
+    return get_chat_response(chat, prompt_improved)
 
 if __name__ == "__main__":
-    content = get_audio_content()
+    text = get_audio_text()
+    # text = ""
+    # with open("transcription_short.txt", "r") as f:
+    #     text = f.read()
+    res = get_imporved_sentences(text)
+    print(res)
+
